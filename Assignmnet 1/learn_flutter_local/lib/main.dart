48ed9802dcd6f98d6f61bd3996a8bb40f1dd4dff
Andrew Ashraf <andrewashraf698@gmail.com>
2024-05-03 00:27:03 +0300

    Hnadling Folders

--- Assignmnet 1/learn_flutter_local/lib/main.dart
+++ Assignmnet 1/learn_flutter_local/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:learn_flutter/view/login_page/login.dart';
-import 'package:learn_flutter/view/profile_page/profile.dart';
-import 'package:learn_flutter/view/register_page/register.dart';
+// import 'package:learn_flutter/view/profile_page/profile.dart';
+// import 'package:learn_flutter/view/register_page/register.dart';
 
 void main() {
-
   runApp(MaterialApp(
     title: 'Flutter Assignment 1',
     theme: ThemeData(
